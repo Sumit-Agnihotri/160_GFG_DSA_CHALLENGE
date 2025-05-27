361c549908ec0502a0a3ae4e369fed1acd436b53
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-05-27 07:58:30 +0530

    160 challenge

---
+++ Challenge 59 Trapping Rain Water.py
@@ -0,0 +1,23 @@
+arr = [3, 0, 1, 0, 4, 0 2]
+
+class Solution:
+    def maxWater(self, arr):
+        left = 1
+        right = len(arr) - 2
+        lMax = arr[left - 1]
+        rMax = arr[right + 1]
+        res = 0
+        while left <= right:
+            if rMax <= lMax:
+                res += max(0, rMax - arr[right])
+                rMax = max(rMax, arr[right])
+                right -= 1
+            else:
+                res += max(0, lMax - arr[left])
+                lMax = max(lMax, arr[left])
+                left += 1
+        return res
+
+s = Solution()
+result = s.maxWater(arr)
+print(result)  # Output: 10
