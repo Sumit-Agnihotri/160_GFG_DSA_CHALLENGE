14d73761a897e79bc09889163d3a7067e61b9946
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-06-16 15:25:05 +0530

    160 challenge

--- Challenge 03 Reverse an Array.py
+++ Challenge 03 Reverse an Array.py
@@ -1,8 +1,12 @@
 arr = [1,4,3,2,6,5]
 class Solution:
     def reverseArray(self, arr):
-        return arr.reverse()
-    
+        left = 0
+        right = len(arr) - 1
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
 s = Solution()
 print(s.reverseArray(arr))
 print(arr)
