15339a4bede15d309ae831355f101a56ad4e213c
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-03 07:35:19 +0530

    160 challenge

---
+++ Challenge 96 K Closest Points to Origin.py
@@ -0,0 +1,12 @@
+points = [[1, 3], [-2, 2], [5, 8], [0, 1]]
+k = 2
+
+from heapq import nsmallest
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return nsmallest(k, points, key = lambda p: p[0] ** 2 + p[1] ** 2)
+
+s = Solution()
+print(s.kClosest(points, k))  # Output: [[0, 1], [-2, 2]]
