c8ace42b1f035c5caabaf2ef1a4cb83ade35075a
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-25 07:53:43 +0530

    160 challenge

--- Challenge 119 0 - 1 Knapsack Problem.py
+++ Challenge 119 0 - 1 Knapsack Problem.py
@@ -3,13 +3,13 @@ val = [1, 2, 3]
 wt = [4, 5, 1]
 
 class Solution:
-    def knapsack(self, w_capacity, val, wt):
-        n = len(val)
-        dp = [0] * (w_capacity + 1)
+    def knapsack(self, W, val, wt):
+        n=len(val)
+        dp=[0]*(W+1)
         for i in range(n):
-            for w in range(w_capacity, wt[i] - 1, -1):
-                dp[w] = max(dp[w], val[i] + dp[w - wt[i]])
-        return dp[w_capacity]
+            for w in range(W,wt[i]-1,-1):
+                dp[w]=max(dp[w],val[i]+dp[w-wt[i]])
+        return dp[W]
 
 s = Solution()
 print(s.knapsack(W, val, wt))  # Output: 3
